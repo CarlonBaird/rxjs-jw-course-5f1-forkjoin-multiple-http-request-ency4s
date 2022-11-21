9e4bb60ac5af2e719b9e68654310a1f7a01a0518
CarlonBaird <carlyestrella@live.com>
2022-11-20 22:11:04 -0400

    1 file modified

--- index.ts
+++ index.ts
@@ -6,4 +6,8 @@ let URL1: string = 'https://random-data-api.com/api/name/random_name';
 
 let URL2: string = 'https://random-data-api.com/api/nation/random_nation';
 
+let URL3: string = 'https://random-data-api.com/api/food/random_food';
+
 const randomName$ = ajax(URL1);
+const randomNation$ = ajax(URL2);
+const randomFood$ = ajax(URL3);

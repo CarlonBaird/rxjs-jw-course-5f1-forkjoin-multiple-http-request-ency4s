d890683a1a1060c8a5eb92ba9fdd20254d7cc3d7
CarlonBaird <carlyestrella@live.com>
2022-11-20 22:51:57 -0400

    1 file modified

--- index.ts
+++ index.ts
@@ -13,15 +13,21 @@ const randomName$ = ajax(URL1);
 const randomNation$ = ajax(URL2);
 const randomFood$ = ajax(URL3);
 
-randomName$.subscribe((ajaxResponse: any) =>
-  console.log(ajaxResponse.response.first_name)
-);
-randomNation$.subscribe((ajaxResponse) =>
-  console.log(ajaxResponse.response.capital)
-);
-randomFood$.subscribe((ajaxResponse) =>
-  console.log(ajaxResponse.response.dish)
-);
+// randomName$.subscribe((ajaxResponse: any) =>
+//   console.log(ajaxResponse.response.first_name)
+// );
+// randomNation$.subscribe((ajaxResponse) =>
+//   console.log(ajaxResponse.response.capital)
+// );
+// randomFood$.subscribe((ajaxResponse) =>
+//   console.log(ajaxResponse.response.dish)
+// );
 
 //forkJoin waits for all the subscriptions to complete and then emit the values provided by each of the input observables
-forkJoin(randomName$, randomNation$, randomFood$).subscribe();
+forkJoin([randomName$, randomNation$, randomFood$]).subscribe(
+  //using destructuring [nameAjax, nationAjax, foodAjax]
+  ([nameAjax, nationAjax, foodAjax]) =>
+    console.log(
+      `${nameAjax.response.first_name} is from ${nationAjax.response.capital} and likes to eat ${foodAjax.response.dish}`
+    )
+);

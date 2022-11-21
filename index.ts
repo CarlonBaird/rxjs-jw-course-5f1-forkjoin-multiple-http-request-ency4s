64647105f9a3a67503b64c7e7cf45afc97f78018
CarlonBaird <carlyestrella@live.com>
2022-11-20 22:41:23 -0400

    1 file modified

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+import { forkJoin } from 'rxjs';
 import { ajax } from 'rxjs/ajax';
 
 //forkJoin : Used to handle multiple HTTP calls
@@ -12,6 +13,15 @@ const randomName$ = ajax(URL1);
 const randomNation$ = ajax(URL2);
 const randomFood$ = ajax(URL3);
 
-randomName$.subscribe((ajaxResponse) => console.log(ajaxResponse));
-randomNation$.subscribe((ajaxResponse) => console.log(ajaxResponse));
-randomFood$.subscribe((ajaxResponse) => console.log(ajaxResponse));
+randomName$.subscribe((ajaxResponse: any) =>
+  console.log(ajaxResponse.response.first_name)
+);
+randomNation$.subscribe((ajaxResponse) =>
+  console.log(ajaxResponse.response.capital)
+);
+randomFood$.subscribe((ajaxResponse) =>
+  console.log(ajaxResponse.response.dish)
+);
+
+//forkJoin waits for all the subscriptions to complete and then emit the values providing by each of the input observables
+forkJoin(randomName$, randomNation$, randomFood$).subscribe();

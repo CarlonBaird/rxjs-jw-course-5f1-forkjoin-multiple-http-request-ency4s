a0c12307328ee7d3f69b7ac454878025af750c7d
CarlonBaird <carlyestrella@live.com>
2022-11-20 22:07:54 -0400

    1 file modified

--- index.ts
+++ index.ts
@@ -1,5 +1,9 @@
 import { ajax } from 'rxjs/ajax';
 
+//forkJoin : Used to handle multiple HTTP calls
+
 let URL1: string = 'https://random-data-api.com/api/name/random_name';
+
+let URL2: string = 'https://random-data-api.com/api/nation/random_nation';
 
 const randomName$ = ajax(URL1);
